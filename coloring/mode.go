cdfadf859b38cf9da54008c959791fe07c9b9cf0
Henry Eklind <henry@karlek.io>
2018-04-20 12:54:50 +0900

    Split coloring package into separate files

---
+++ coloring/mode.go
@@ -0,0 +1,38 @@
+package coloring
+
+// Mode determines the coloring method.
+type Mode int
+
+const (
+	// Modulo determines the coloring scheme based on the modulo of the iteration.
+	Modulo Mode = iota
+	// IterationCount determines the coloring scheme based on the length of the orbit.
+	IterationCount
+	// OrbitLength colors the orbit in a gradient from beginning to end.
+	OrbitLength
+	// VectorField colors the angles between the points in an orbit.
+	VectorField
+	// Path linearly interpolates between the points in the path.
+	Path
+	// Image colors the orbits based on a reference image.
+	Image
+)
+
+func (m Mode) String() string {
+	switch m {
+	case VectorField:
+		return "VectorField"
+	case Modulo:
+		return "Modulo"
+	case IterationCount:
+		return "IterationCount"
+	case OrbitLength:
+		return "OrbitLength"
+	case Path:
+		return "Path"
+	case Image:
+		return "Image"
+	default:
+		return "fail"
+	}
+}
